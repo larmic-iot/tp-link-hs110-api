f65c5833d33cef0e4ba41bbd393e2ee2b0ae2a94
Lars Michaelis <mail@larmic.de>
2020-08-18 17:33:18 +0200

    add first test rest api handler

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module tp-link-hs110-api
 
 go 1.15
+
+require github.com/gorilla/mux v1.7.4 // indirect
--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 
 	"tp-link-hs110-api/client"
 )
@@ -13,8 +16,24 @@ const (
 func main() {
 	log.Println("Hello tp-link-hs110-api!")
 
-	fridgeClient := client.NewTpLinkHS110Client("10.0.0.210", printDebug)
+	//fridgeClient := client.NewTpLinkHS110Client("10.0.0.78", printDebug)
 
-	log.Printf("Request info: %s", fridgeClient.RequestInfo())
-	log.Printf("Request energy: %s", fridgeClient.RequestCurrentEnergyStatistics())
+	//log.Printf("Request info: %s", fridgeClient.RequestInfo())
+	//log.Printf("Request energy: %s", fridgeClient.RequestCurrentEnergyStatistics())
+	//log.Printf("Request energy: %s", fridgeClient.RequestSwitchOff())
+	//log.Printf("Request energy: %s", fridgeClient.RequestSwitchOn())
+
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/api/hello/{ip}", testHandler).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["ip"]
+
+	socketClient := client.NewTpLinkHS110Client(key, printDebug)
+
+	w.Write([]byte(socketClient.RequestInfo()))
 }
