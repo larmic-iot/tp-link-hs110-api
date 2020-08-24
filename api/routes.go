300c7d6e18c8dfb14283ddbd2586b2dc5342eab9
Lars Michaelis <mail@larmic.de>
2020-08-24 13:37:35 +0200

    add logger.go

--- api/routes.go
+++ api/routes.go
@@ -21,11 +21,16 @@ func NewRouter() *mux.Router {
 	router.NotFoundHandler = Handle404()
 
 	for _, route := range routes {
+		var handler http.Handler
+
+		handler = route.HandlerFunc
+		handler = Logger(handler, route.Name)
+
 		router.
 			Methods(route.Method).
 			Path(route.Pattern).
 			Name(route.Name).
-			Handler(route.HandlerFunc)
+			Handler(handler)
 	}
 
 	return router
